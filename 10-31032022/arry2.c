a6ff9e4327a043488facf6249544312552526326
julianoV13 <juliano.vitoriano2002@gmail.com>
2022-03-31 12:02:15 -0300

    array

---
+++ 10-31032022/arry2.c
@@ -0,0 +1,14 @@
+/*programa que realiza a soma usando array*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "calculos.h"
+
+void main(){
+
+    int v[10]={1,23,53,4,9,4,6,3,56};
+    soma(v,10);
+    maiorvalor(v,10);
+
+}
